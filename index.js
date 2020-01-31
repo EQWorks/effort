dfb7cea32c41651f4218c269a8cd0b06824e1975
woozyking <runzhou.li@gmail.com>
2020-01-31 16:54:45 -0500

    support split daily across multiple projects, and safeguard against 0 percent portions

--- index.js
+++ index.js
@@ -19,17 +19,20 @@ const COLUMNS = Object.freeze([
   // 'Tags',
 ])
 
-// default distribution
+// default distributions
 const TASKS = Object.freeze({
   'Research/Design': 0.1,
   Development: 0.7,
   'QA/Maintenance': 0.1,
   'Admin/Ops': 0.1,
 })
+const PROJECTS = Object.freeze({
+  Locus: 1,
+  ATOM: 0,
+})
 
 const HOURS = 8 // default daily work hours per user
 const START = '09:00:00' // default work start
-const PROJECT = 'Locus' // default project
 const TZ = 'America/Toronto'
 const HD = new Holidays('CA', 'ON')
 
@@ -37,28 +40,31 @@ const HD = new Holidays('CA', 'ON')
 const genDaily = ({
   Email,
   day,
-  Project = PROJECT,
   hours = HOURS,
   start = START,
+  projects = PROJECTS,
   tasks = TASKS,
 }) => {
   const startDate = (day ? moment.tz(day, TZ) : moment.tz(TZ)).format('YYYY-MM-DD')
   let startTime = moment.tz(`${startDate} ${start}`, TZ).utc()
   const durations = {}
 
-  shuffle(Object.entries(tasks)).forEach(([Task, portion]) => {
-    const d = moment.duration(hours * portion * (1 + (Math.random() * 2 - 1) * 0.1), 'hours')
-    durations[Task] = {
-      startTime: startTime.format('HH:mm:ss'),
-      Duration: moment.utc(d.asMilliseconds()).format('HH:mm:ss'),
-    }
-    startTime.add(d)
+  shuffle(Object.entries(projects)).filter(([, pportion]) => pportion > 0).forEach(([Project, pportion]) => {
+    shuffle(Object.entries(tasks)).filter(([, portion]) => portion > 0).forEach(([Task, portion]) => {
+      const err = (1 + (Math.random() * 2 - 1) * 0.1) // +/- 10% error
+      const d = moment.duration(hours * pportion * portion * err, 'hours')
+      durations[`${Project}::${Task}`] = {
+        startTime: startTime.format('HH:mm:ss'),
+        Duration: moment.utc(d.asMilliseconds()).format('HH:mm:ss'),
+      }
+      startTime.add(d)
+    })
   })
 
-  const rows = Object.entries(durations).map(([Task, { startTime, Duration }]) => ({
+  const rows = Object.entries(durations).map(([key, { startTime, Duration }]) => ({
     Email,
-    Project,
-    Task,
+    Project: key.split('::')[0],
+    Task: key.split('::')[1],
     'Start Date': startDate,
     'Start Time': startTime,
     Duration,
@@ -74,7 +80,8 @@ const genYear = ({ year, Email }) => {
 
   console.log(COLUMNS.map(c => `"${c}"`).join(','))
   for (let day of range.by('day')) {
-    // skip weekends on public holidays
+    // skip weekends and public holidays
+    // TODO: skip given vacation days too
     if (HD.isHoliday(day.toDate()) || [6, 7].includes(day.isoWeekday())) {
       continue
     }
