1e791287d5ee32a7727acb190a616431b095f7d5
woozyking <runzhou.li@gmail.com>
2020-02-14 09:42:06 -0500

    per range error for more realistic estimations

--- index.js
+++ index.js
@@ -19,39 +19,42 @@ const COLUMNS = Object.freeze([
   // 'Tags',
 ])
 
-// default distributions
-const TASKS = Object.freeze({
-  'Research/Design': 0.1,
-  Development: 0.7,
-  'QA/Maintenance': 0.1,
-  'Admin/Ops': 0.1,
-})
-const PROJECTS = Object.freeze({
-  Locus: 1,
-  ATOM: 0,
-})
-
 const HOURS = 8 // default daily work hours per user
 const START = '09:00:00' // default work start
 const TZ = 'America/Toronto'
 const HD = new Holidays('CA', 'ON')
 
 
+const genError = () => 1 + (Math.random() * 2 - 1) * 0.111
+
+const redistributePortion = ({ tasks, margin }) => {
+  const rt = { ...tasks }
+  rt.Development = rt.Development * margin
+  const rem = (1 - rt.Development) / 3
+  ;['Research/Design', 'QA/Maintenance', 'Admin/Ops'].forEach((t) => {
+    rt[t] = rem
+  })
+  return rt
+}
+
 const genDaily = ({
   Email,
   day,
   hours = HOURS,
   start = START,
-  projects = PROJECTS,
-  tasks = TASKS,
+  projects = {
+    LOCUS: 1,
+    ATOM: 0,
+  },
 }) => {
   const startDate = (day ? moment.tz(day, TZ) : moment.tz(TZ)).format('YYYY-MM-DD')
   let startTime = moment.tz(`${startDate} ${start}`, TZ).utc()
   const durations = {}
 
   shuffle(Object.entries(projects)).filter(([, pportion]) => pportion > 0).forEach(([Project, pportion]) => {
-    shuffle(Object.entries(tasks)).filter(([, portion]) => portion > 0).forEach(([Task, portion]) => {
-      const err = (1 + (Math.random() * 2 - 1) * 0.1) // +/- 10% error
+    shuffle(Object.entries(rt)).filter(([, portion]) => portion > 0).forEach(([Task, portion]) => {
+      // per-task, per-day error
+      const err = genError()
       const d = moment.duration(hours * pportion * portion * err, 'hours')
       durations[`${Project}::${Task}`] = {
         startTime: startTime.format('HH:mm:ss'),
@@ -77,7 +80,18 @@ const logHeader = () => {
   console.log(COLUMNS.map(c => `"${c}"`).join(','))
 }
 
-const genRange = ({ start, end, Email, vacations = [] }) => {
+const genRange = ({
+  start,
+  end,
+  Email,
+  vacations = [],
+  tasks = {
+    Development: 0.7,
+    'Research/Design': 0.1,
+    'QA/Maintenance': 0.1,
+    'Admin/Ops': 0.1,
+  },
+}) => {
   const range = moment.range(
     moment.tz(start, TZ).startOf('day'),
     moment.tz(end, TZ).endOf('day'),
@@ -86,6 +100,9 @@ const genRange = ({ start, end, Email, vacations = [] }) => {
     moment.tz(start, TZ).startOf('day'),
     moment.tz(end, TZ).endOf('day'),
   ))
+  // per-range estimation error to redistribute task portions
+  const margin = genError()
+  const rt = redistributePortion({ tasks, margin })
 
   for (let day of range.by('day')) {
     // skip weekends and public holidays
@@ -96,21 +113,14 @@ const genRange = ({ start, end, Email, vacations = [] }) => {
     if (vacays.find(r => r.contains(day))) {
       continue
     }
-    genDaily({ Email, day }).forEach((v) => {
+    genDaily({ Email, day, tasks: rt }).forEach((v) => {
       console.log(Object.values(v).map(v => `"${v}"`).join(','))
     })
   }
 }
 
 if (require.main === module) {
+  const peepo = []
   logHeader()
-  genRange({
-    start: '2019-01-01',
-    end: '2019-12-31',
-    Email: 'ianecc@eqworks.com',
-    vacations: [
-      { start: '2019-10-23', end: '2019-10-30' },
-      { start: '2019-12-23', end: '2019-01-03' },
-    ],
-  })
+  peepo.forEach(p => { genRange(p) })
 }
